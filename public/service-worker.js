4ec7c8f8bf9683fac86b191ab6e6d77ea3f4949f
SathyaThangam <sathya@codingmart.com>
2019-03-19 16:49:33 +0530

    info alignments

--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.86deb947b80fcd00f2da2ad0739fa357.js"
+  "/precache-manifest.e5f7a83a5cc04507d5837e453d391a32.js"
 );
 
 workbox.clientsClaim();
